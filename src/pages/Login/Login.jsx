87c964b9f527aabe783bd0b7074bbc953b504fec
DoAnhThu832004 <adugarux4@gmail.com>
2025-11-22 21:56:44 +0700

    gg

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -24,7 +24,7 @@ export default function Login() {
     try {
       const result = await loginAPI({ email, password });
       localStorage.setItem("login", "true");
-      localStorage.setItem("userId", result.id);
+      localStorage.setItem("userId", result.id);gggg
 
       navigate("/home");
     } catch (error) {
